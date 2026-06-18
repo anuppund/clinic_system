e82d6f6a40d6b51aa677261e0ff080f28cd9b8fa
anuppund <95672770+anuppund@users.noreply.github.com>
2026-06-19 03:37:34 +0530

    Updated axios.ts
    updated render api url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
-export const api = axios.create({ baseURL: 'http://localhost:8000' });
+export const api = axios.create({ baseURL: 'https://clinic-api-zltu.onrender.com/' });
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   if (token) config.headers.Authorization = `Bearer ${token}`;
